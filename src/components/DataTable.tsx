fc0940efcc887607682cd233aaebc6a264570608
hiteshchoubisa <hitesh.choubisa123@gmail.com>
2025-10-04 22:53:47 +0530

    updated users

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -66,7 +66,7 @@ export default function DataTable<T>({
             return (
               <div
                 key={key}
-                className="relative rounded-lg border border-black/10 bg-white p-3"
+                className={cardRenderer ? "" : "relative rounded-lg border border-black/10 bg-white p-3"}
               >
                 {cardRenderer ? (
                   cardRenderer(row)
